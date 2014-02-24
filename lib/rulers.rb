d0c08aa7b1f0bce168ace94971213c3e01ae732c
cclaiborne <claiborne206@gmail.com>
2014-02-23 17:37:08 -0800

    Adding controller for chapter 2

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,11 +1,11 @@
+# rulers/lib/rulers.rb
 require "rulers/version"
+require "rulers/routing"
 
 module Rulers
-  class Application
-    def call(env)
-    `echo debug > debug.txt`;
-      [200, {'Content-Type' => 'text/html'},
-        ["Hello from Ruby on Rulers!"]]
-    end
-  end
-end
+    class Application
+      def call(env)
+        klass, act = get_controller_and_action(env)
+        controller = klass.new(env)
+        text = controller.send(act)
+        [200, {'Content-Type'}]
